4c959923ba656bb0b003db292630ea95977d4f01
ASUS PC <raymondgatacelo13@gmail.com>
2023-05-04 04:10:46 +0800

    fix avatar name

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -36,7 +36,7 @@ export function Nav(props: any) {
         </Heading>
       </Flex>
       <Avatar
-        name={`${user?.Fname} ${user?.Lname}`}
+        name={`${user?.fName} ${user?.lName}`}
         accessType={user.JobTitle || "admin"}
       />
     </Flex>
